4b69e7beeac480e9e89698636b5ed6f29b86e5d4
Nahuel Chamorro <118007578+nahucham18@users.noreply.github.com>
2023-07-14 10:21:25 -0300

    Creacion de NavBar.jsx y Menu.jsx Basico

---
+++ components/Menu/Menu.jsx
@@ -0,0 +1,10 @@
+export default function Menu () {
+    return (
+        <ul className="flex justify-around">
+            <li className="cursor-pointer hover:text-[#FFFFFF] border-b-[1px] border-[#11111100] hover:border-[#FFFFFF]">Comidas</li>
+            <li className="cursor-pointer hover:text-[#FFFFFF] border-b-[1px] border-[#11111100] hover:border-[#FFFFFF]">Calendario</li>
+            <li className="cursor-pointer hover:text-[#FFFFFF] border-b-[1px] border-[#11111100] hover:border-[#FFFFFF]">Favoritas</li>
+            <li className="cursor-pointer hover:text-[#FFFFFF] border-b-[1px] border-[#11111100] hover:border-[#FFFFFF]">Mas..</li>
+        </ul>
+    )
+}
---
+++ components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import Menu from "../Menu/Menu";
+
+export default function NavBar () {
+    return (
+        <nav className="flex justify-around items-center text-[#E9E5D6] bg-[#464E2E] h-[10vh] py-[1rem]">
+            <div className="w-[10%] cursor-pointer hover:text-[#FFFFFF]">Logo</div>
+            <div className=" w-[50%] ">
+                <Menu/>
+            </div>
+            <div className="flex justify-center w-[25%] cursor-pointer hover:text-[#FFFFFF]">Usuario</div>
+        </nav>
+    )
+}
